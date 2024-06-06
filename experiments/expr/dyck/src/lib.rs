bf973d50b7074bc4bd2e2b5cfcb05463adbfadd5
Remy Pierre Bushnell Clarke <clarke.remy@gmail.com>
2024-06-07 03:03:58 +0900

    Added Basic Dyck Structure Zipper ops.

--- experiments/expr/dyck/src/lib.rs
+++ experiments/expr/dyck/src/lib.rs
@@ -15,6 +15,7 @@ use core::{
   iter::{Extend, IntoIterator, Iterator},
   option::Option,
   result::Result,
+  slice,
 };
 use num_bigint::{BigInt, BigUint, ToBigInt};
 use std::default;
@@ -86,7 +87,7 @@ impl<L> Dyck<L> {
 
   fn new(path: &[u32], leaves: Shared<[L]>) -> Option<Self> {
     if path.len() * u32::BITS as usize > PathInt::BITS as usize {
-      let mut v: Vec<u32> = path.into_iter().rev().copied().collect();
+      let v: Vec<u32> = path.into_iter().rev().copied().collect();
       let path_bui = BigUint::new(v);
       UnboundedDyck::new(path_bui, leaves).map(Dyck::UnboundedDyck)
     } else {
@@ -100,68 +101,194 @@ impl<L> Dyck<L> {
   }
 }
 
-struct DyckZipper<'a, L> {
-  path_offset: usize,
-  //? path_len: usize, // to slice
-  //? depth  // to avoid masking off top when doing popcnt check
-  view_of: &'a Dyck<L>,
+#[derive(Clone, Copy)]
+pub(crate) struct SubtreeSlice {
+  /// the position of the terminating __0__ bit after the last leaf
+  pub(crate) terminal: u8,
+  pub(crate) head: u8,
 }
+impl SubtreeSlice {
+  fn is_leaf(&self) -> bool {
+    1 == self.terminal - self.head
+  }
+  fn left_subtree_head(self, structure: u64) -> u64 {
+    let slice = (structure & (1_u64 << self.terminal - 2).wrapping_sub(1)) >> self.head - 1;
 
-impl<'a, L> Clone for DyckZipper<'a, L> {
-  fn clone(&self) -> Self {
-    // Safety: we are implementing a Copy type
-    unsafe { core::ptr::read(self) }
+    left_branch_impl::u64::left_branch(slice) << self.head - 1
   }
 }
-impl<'a, L> core::marker::Copy for DyckZipper<'a, L> {}
 
-// impl<'a, L> DyckZipper<'a, L> {
-//   fn go_left(self)->Option<Self>{
-//   }
-// }
+pub struct DyckStructureZipperU64 {
+  structure: u64,
+  current_depth: u8,
+  stack: [SubtreeSlice; DyckStructureZipperU64::STACK_LEN],
+}
 
+impl DyckStructureZipperU64 {
+  const STACK_LEN: usize = u64::BITS as usize / 2;
 
+  pub fn new(structure: u64) -> Option<Self> {
+    if let 0 = structure {
+      return Option::None;
+    }
 
-pub trait DyckPathFindLeftBranch where usize : From<Self::Offset>{
-  type Offset;  
+    let mut stack = [SubtreeSlice { terminal: 0, head: 0 }; Self::STACK_LEN];
+    stack[0].terminal = 1 << u64::BITS.wrapping_sub(structure.leading_zeros());
+
+    Option::Some(Self { structure, current_depth: 0, stack })
+  }
+
+  pub fn decend_left(&mut self) -> bool {
+    let cur = self.stack[self.current_depth as usize];
+    if cur.is_leaf() {
+      return false;
+    }
+    let l_head @ (1..=u64::MAX) = cur.left_subtree_head(self.structure) else {
+      return false;
+    };
+
+    self.current_depth += 1;
+    self.stack[self.current_depth as usize] = SubtreeSlice { terminal: cur.terminal, head: l_head.trailing_zeros() as u8 };
+
+    true
+  }
+
+  pub fn decend_right(&mut self) -> bool {
+    let cur = self.stack[self.current_depth as usize];
+    if cur.is_leaf() {
+      return false;
+    }
+    let l_head @ (1..=u64::MAX) = cur.left_subtree_head(self.structure) else {
+      return false;
+    };
+
+    self.current_depth += 1;
+    self.stack[self.current_depth as usize] = SubtreeSlice { terminal: l_head.trailing_zeros() as u8, head: cur.head + 1 };
+
+    true
+  }
+
+  pub unsafe fn switch_right_unchecked(&mut self) {
+    let Self { structure, current_depth, stack } = self;
+    core::debug_assert!(!core::matches!(*structure, 0 | 1));
+    core::debug_assert_ne!(*current_depth, 0);
+
+    let prev = unsafe { *stack.get_unchecked(*current_depth as usize - 1) };
+    let cur = unsafe { stack.get_unchecked_mut(*current_depth as usize) };
+
+    // is left
+    core::debug_assert_eq!(prev.terminal, cur.terminal);
+    // not right
+    core::debug_assert_ne!(prev.head, cur.head + 1);
+
+    *cur = if cur.is_leaf() { SubtreeSlice { terminal: cur.terminal - 1, head: prev.head + 1 } } else { SubtreeSlice { terminal: cur.head, head: prev.head + 1 } };
+  }
+  pub fn switch_right(&mut self) -> bool {
+    let Self { structure, current_depth, stack } = self;
+    if 1 >= *structure || 0 == *current_depth {
+      return false;
+    }
+
+    // Safety: The current depth is > 0
+    let prev = unsafe { stack.get_unchecked(*current_depth as usize - 1) };
+    let cur = unsafe { stack.get_unchecked(*current_depth as usize) };
+
+    // check if on right
+    if prev.terminal != cur.terminal {
+      return false;
+    }
+
+    unsafe { self.switch_right_unchecked() }
+
+    true
+  }
+  pub unsafe fn switch_left_unchecked(&mut self) {
+    let Self { structure, current_depth, stack } = self;
+    core::debug_assert!(!core::matches!(*structure, 0 | 1));
+    core::debug_assert_ne!(*current_depth, 0);
+
+    let prev = unsafe { *stack.get_unchecked(*current_depth as usize - 1) };
+    let cur = unsafe { stack.get_unchecked_mut(*current_depth as usize) };
+
+    // not left
+    core::debug_assert_ne!(prev.terminal, cur.terminal);
+    // is right
+    core::debug_assert_eq!(prev.head, cur.head + 1);
+
+    *cur = if cur.is_leaf() { SubtreeSlice { terminal: prev.terminal, head: cur.head + 1 } } else { SubtreeSlice { terminal: prev.terminal, head: cur.terminal } };
+  }
+  pub fn switch_left(&mut self) -> bool {
+    let Self { structure, current_depth, stack } = self;
+    if 1 >= *structure || 0 == *current_depth {
+      return false;
+    }
+
+    // Safety: The current depth is > 0
+    let prev = unsafe { stack.get_unchecked(*current_depth as usize - 1) };
+    let cur = unsafe { stack.get_unchecked(*current_depth as usize) };
+
+    // check if on the left
+    if prev.head != cur.head + 1 {
+      return false;
+    }
+
+    unsafe { self.switch_left_unchecked() };
+
+    true
+  }
+
+  pub unsafe fn index_unchecked(&self, tree_offset: u8) -> usize {
+    core::debug_assert_ne! {self.structure&(1 << tree_offset as u32), 0}
+    (self.structure ^ self.structure & (1 << tree_offset as u32) - 1).count_ones() as usize
+  }
+
+  pub fn current_leaf_index_range(&self) -> core::ops::Range<usize> {
+    unsafe {
+      let cur = self.stack.get_unchecked(self.current_depth as usize);
+      self.index_unchecked(cur.terminal - 1)..self.index_unchecked(cur.head) + 1
+    }
+  }
+
+  /// Index of the first in the current scope
+  pub fn cur_first_index(&self) -> usize {
+    let Self { current_depth, stack, .. } = self;
+    unsafe { self.index_unchecked(stack[*current_depth as usize].terminal - 1) }
+  }
+}
+
+pub trait DyckPathFindLeftBranch
+where
+  usize: From<Self::Offset>,
+{
+  type Offset;
   fn left_branch(self) -> Self::Offset;
 }
 
 // this can be refactored to be less repitive, but then I would have to use some traits, I'm avoiding the complexity for now
 pub(crate) mod left_branch_impl {
-  macro_rules! left_branch {($($INT:ident)+) => {$(
-    pub(crate)mod $INT {
-      pub(crate)fn left_branch(mut structure: $INT) -> $INT {
-        if structure<=1 { return 0;}
-
-        if 0b10 & structure == 0b10 {
-          return 0b100;
+  pub(crate) mod u64 {
+    pub(crate) fn left_branch(structure: u64) -> u64 {
+      if structure <= 1 {
+        return 0;
+      }
+      if 0b10 & structure == 0b10 {
+        return 0b100;
+      }
+      let mut left_splits = !structure & structure << 1 & structure << 2;
+      loop {
+        let trailing = left_splits.trailing_zeros();
+        let current = 1 << trailing;
+        if let 1 = left_splits.count_ones() {
+          return current >> 1;
         }
-
-        /* 011 bit pattern represents a "non-trivial" split, where 0 is the left node
-           the bit one past the end will always be a "non-trivial" split,
-        */
-        let mut left_splits = !structure & structure << 1 & structure << 2;
-      
-        // moving from right to left
-        loop {
-          let trailing = left_splits.trailing_zeros();
-          let current = 1<<trailing;
-          if let 1 = left_splits.count_ones() { return current >> 1 }
-          let tmp = structure >> trailing;
-          
-          if ($INT::BITS-tmp.leading_zeros() + 1).wrapping_sub(tmp.count_ones() * 2) == 0 {
-            return current;
-          }
-          
-          // clear right most candidate
-          left_splits ^= current;
-          
+        let tmp = structure >> trailing;
+        if (u64::BITS - tmp.leading_zeros() + 1).wrapping_sub(tmp.count_ones() * 2) == 0 {
+          return current;
         }
+        left_splits ^= current;
       }
     }
-  )+};}
-  left_branch! {u8 u16 u32 u64 u128 usize}
+  }
 
   // we might be interested in testing the 512 fixed size, as that makes for 256 leaves max, which means the backing Vec can be indexed by a byte.
   // unfortunately, this appears to be as slow as the unbounded version, likely due to a subpar implementation on the U512 type if the issue is the same as Adam had.
@@ -169,60 +296,60 @@ pub(crate) mod left_branch_impl {
   pub(crate) mod u512 {
     use crate::*;
     pub(crate) use bnum::types::U512;
-    
-    pub(crate) fn left_branch(mut structure: U512) -> Option<core::num::NonZeroU32> {
-      let offset = 'find_offset : {
 
+    pub(crate) fn left_branch(mut structure: U512) -> Option<core::num::NonZeroU32> {
+      let offset = 'find_offset: {
         if structure.count_ones() <= 1 {
           break 'find_offset 0;
         }
-        
+
         #[allow(non_upper_case_globals)]
         const u_0b10: U512 = U512::TWO;
         #[allow(non_upper_case_globals)]
         const u_0b100: U512 = U512::FOUR;
-        
+
         if structure.bit(1) && !structure.bit(0) {
           break 'find_offset 2;
         }
-        
+
         /* 011 bit pattern represents a "non-trivial" split, where 0 is the left node
         the bit one past the end will always be a "non-trivial" split,
         */
-        
-        let mut left_splits = U512::ONE << structure.bits()+1;
-        
+
+        let mut left_splits = U512::ONE << structure.bits() + 1;
+
         // [0]11
         left_splits -= U512::ONE;
-        left_splits ^= &structure; 
-        
+        left_splits ^= &structure;
+
         // [0]11 & 0[1]1 => [01]1
         structure <<= 1;
-        left_splits &= &structure; 
-        
+        left_splits &= &structure;
+
         // [01]1 & 01[1] => [011]
         structure <<= 1;
         left_splits &= &structure;
-        
+
         // reset
         structure >>= 2;
         let mut current_shift = 0;
-        
+
         // moving from right to left
         loop {
           let trailing = left_splits.trailing_zeros();
-          if let 1 = left_splits.count_ones() { break 'find_offset trailing- 1 }
-          
+          if let 1 = left_splits.count_ones() {
+            break 'find_offset trailing - 1;
+          }
+
           structure >>= trailing - current_shift;
-          
+
           if (structure.bits() + 1).wrapping_sub(structure.count_ones() * 2) == 0 {
             break 'find_offset trailing;
           }
-          
+
           // clear right most candidate
-          left_splits ^= U512::ONE<<trailing;
+          left_splits ^= U512::ONE << trailing;
           current_shift = trailing;
-          
         }
       };
       core::num::NonZeroU32::new(offset)
@@ -233,58 +360,59 @@ pub(crate) mod left_branch_impl {
   pub(crate) mod big_uint {
     use crate::*;
     pub(crate) fn left_branch(mut structure: BigUint) -> Option<core::num::NonZeroU64> {
-      let offset = 'find_offset : {
-      if structure.count_ones() <= 1 {
-        break 'find_offset 0;
-        // return BigUint::ZERO;
-      }
-      if structure.bit(1) && !structure.bit(0) {
-        // return BigUint::from(0b100_u32);
-        break 'find_offset 2;
-      }
-
-      /* 011 bit pattern represents a "non-trivial" split, where 0 is the left node
-         the bit one past the end will always be a "non-trivial" split
-      */
-      let u_0b1 = BigUint::from(1_u32);
-  
-      let mut left_splits = &u_0b1 << structure.bits()+1;
-
-      // [0]11
-      left_splits -= &u_0b1;
-      left_splits ^= &structure; 
-      
-      // [0]11 & 0[1]1 => [01]1
-      structure <<= 1;
-      left_splits &= & structure; 
-
-      // [01]1 & 01[1] => [011]
-      structure <<= 1;
-      left_splits &= & structure;
-      
-      // reset
-      structure >>= 2;
-      let mut current_shift = 0;
-
-      // moving from right to left
-      loop {
-        let trailing = left_splits.trailing_zeros().expect("TRAILING ZEROS");
-        
-        if let 1 = left_splits.count_ones() { 
-        break 'find_offset trailing-1 }
-          // return u_0b1 << trailing-1 }
-        
-        structure >>= trailing - current_shift;
-        
-        if (structure.bits() + 1).wrapping_sub(structure.count_ones() * 2) == 0 {
-        break 'find_offset trailing;
-          // return u_0b1<<trailing;
+      let offset = 'find_offset: {
+        if structure.count_ones() <= 1 {
+          break 'find_offset 0;
+          // return BigUint::ZERO;
         }
-        
-        // clear right most candidate
-        left_splits.set_bit(trailing as u64, false);
-        current_shift = trailing;
-      }
+        if structure.bit(1) && !structure.bit(0) {
+          // return BigUint::from(0b100_u32);
+          break 'find_offset 2;
+        }
+
+        /* 011 bit pattern represents a "non-trivial" split, where 0 is the left node
+           the bit one past the end will always be a "non-trivial" split
+        */
+        let u_0b1 = BigUint::from(1_u32);
+
+        let mut left_splits = &u_0b1 << structure.bits() + 1;
+
+        // [0]11
+        left_splits -= &u_0b1;
+        left_splits ^= &structure;
+
+        // [0]11 & 0[1]1 => [01]1
+        structure <<= 1;
+        left_splits &= &structure;
+
+        // [01]1 & 01[1] => [011]
+        structure <<= 1;
+        left_splits &= &structure;
+
+        // reset
+        structure >>= 2;
+        let mut current_shift = 0;
+
+        // moving from right to left
+        loop {
+          let trailing = left_splits.trailing_zeros().expect("TRAILING ZEROS");
+
+          if let 1 = left_splits.count_ones() {
+            break 'find_offset trailing - 1;
+          }
+          // return u_0b1 << trailing-1 }
+
+          structure >>= trailing - current_shift;
+
+          if (structure.bits() + 1).wrapping_sub(structure.count_ones() * 2) == 0 {
+            break 'find_offset trailing;
+            // return u_0b1<<trailing;
+          }
+
+          // clear right most candidate
+          left_splits.set_bit(trailing as u64, false);
+          current_shift = trailing;
+        }
       };
       core::num::NonZeroU64::new(offset)
     }
@@ -325,13 +453,12 @@ fn all_trees() -> Vec<Int> {
 
 #[test]
 fn test_for_u64() {
-
   let trees = all_trees();
   std::println!("count : {}", all_trees().len());
   let now = std::time::Instant::now();
-    for each in trees {
-      // std::print!("{each:016b}\t{:016b}\n", 
-        core::hint::black_box(left_branch_impl::u64::left_branch(each as u64))
+  for each in trees {
+    // std::print!("{each:032b}\t{:016b}\n",
+    core::hint::black_box(left_branch_impl::u64::left_branch(each as u64))
       // )
       ;
   }
@@ -339,31 +466,29 @@ fn test_for_u64() {
 }
 #[test]
 fn test_for_biguint() {
-  
   let trees = all_trees();
   let now = std::time::Instant::now();
-    for each in trees {
-      // std::print!("{each:032b}\t{:?}\n", 
-        core::hint::black_box(
+  for each in trees {
+    // std::print!("{each:032b}\t{:?}\n",
+    core::hint::black_box(
           left_branch_impl::big_uint::left_branch(BigUint::from(each))
       )
       // )
       ;
-    }
+  }
   std::println!("time : {} micros", now.elapsed().as_micros())
 }
 #[test]
 fn test_for_u512() {
-  
   let trees = all_trees();
   let now = std::time::Instant::now();
-    for each in trees {
-      // std::print!("{each:016b}\t{}\n", 
-        core::hint::black_box(
+  for each in trees {
+    // std::print!("{each:032b}\t{}\n",
+    core::hint::black_box(
           left_branch_impl::u512::left_branch(bnum::types::U512::from_digit(each as u64))
       )
       // )
       ;
-    }
+  }
   std::println!("time : {} micros", now.elapsed().as_micros())
 }

1468c188ca485c0af0221e53b1609f4486fe1c81
Adam-Vandervorst <adam.vandervorst@gmail.com>
2024-10-29 23:40:50 +0100

    Add sequential baseline

--- benchmarks/parmap/src/main.rs
+++ benchmarks/parmap/src/main.rs
@@ -48,7 +48,7 @@ fn setup() -> &'static mut ZipperHead<'static, 'static, ()> {
 
 fn parallel_map() {
     const k: u64 = 1_000_000_000;
-    const TC: u32 = 48;
+    const TC: u32 = 64;
     let pool = ThreadPoolBuilder::new().num_threads(TC as usize).build().unwrap();
     static mut counter: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
 
@@ -111,7 +111,46 @@ fn parallel_map() {
     // }
 }
 
+fn sequential_map() {
+    // const k: u64 = 1000;
+    const k: u64 = 1_000_000_000;
+
+    let zh = setup();
+
+    let mut buildz = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&[0]) };
+    buildz.graft_map(BytesTrieMap::range::<true, u64>(0, k, 1, ()));
+    drop(buildz);
+    let mut dz = unsafe { zh.read_zipper_at_path_unchecked(&[0]) };
+    let mut oz = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&[1]) };
+
+    let t0 = Instant::now();
+    loop {
+        if dz.to_next_val().is_none() { break }
+        // println!("tp {:?}", dz.path());
+        let mut buffer = [0; 8];
+        for (s, t) in dz.path().iter().rev().zip(buffer.iter_mut().rev()) {
+            *t = *s;
+        }
+        let v = u64::from_be_bytes(buffer);
+        oz.descend_to(dz.path());
+        let vr = (v as f64).sqrt() as u64;
+        // println!("calculated f({v}) = {vr}");
+        oz.descend_to(&vr.to_be_bytes()[..]);
+        oz.set_value(());
+        oz.reset();
+    }
+    drop(oz);
+    println!("processing took {} micros", t0.elapsed().as_micros());
+
+    let mut rz = unsafe { zh.read_zipper_at_path_unchecked(&[]) };
+    println!("result count: {}", rz.val_count());
+    // while let Some(_) = rz.to_next_val() {
+    //     println!("o {:?}", rz.path());
+    // }
+}
+
 
 fn main() {
-    parallel_map();
+    sequential_map();
+    // parallel_map();
 }

a75f2923597467a9857c5ac02b158c7433ab5e2b
Luke Peterson <luke@infoplatter.com>
2024-11-06 15:18:52 +0900

    Fixing warnings in parmap benchmark

--- benchmarks/parmap/src/main.rs
+++ benchmarks/parmap/src/main.rs
@@ -1,12 +1,11 @@
 use std::sync::atomic::Ordering;
-use std::thread::sleep;
-use std::time::{Duration, Instant};
-use rayon::{ThreadPool, ThreadPoolBuilder};
-use pathmap::zipper::{Zipper, ReadZipperUntracked, WriteZipperUntracked, ZipperIteration, ZipperHead, ZipperAbsolutePath};
+use std::time::Instant;
+use rayon::ThreadPoolBuilder;
+use pathmap::zipper::{Zipper, ReadZipperUntracked, ZipperIteration, ZipperAbsolutePath};
 use pathmap::trie_map::BytesTrieMap;
 use pathmap::zipper::WriteZipper;
 
-const K: u64 = 1_000_000_000;
+const K: u64 = 1_000_000_0;
 
 fn homo<F: FnMut(u32, &mut ReadZipperUntracked<()>) -> ()>(at_least: u32, rz: &mut ReadZipperUntracked<()>, f: &mut F) {
     rz.descend_until();
@@ -36,139 +35,135 @@ fn homo<F: FnMut(u32, &mut ReadZipperUntracked<()>) -> ()>(at_least: u32, rz: &m
     }
 }
 
-static mut M: Option<Box<BytesTrieMap<()>>> = None;
-static mut ZH: Option<Box<ZipperHead<()>>> = None;
-
-fn setup() -> &'static mut ZipperHead<'static, 'static, ()> {
-    unsafe {
-        M = Some(Box::new(BytesTrieMap::new()));
-        ZH = Some(Box::new(M.as_deref_mut().unwrap().zipper_head()));
-        ZH.as_mut().unwrap()
-    }
-}
-
+#[allow(unused)]
 fn parallel_map() {
     const TC: u32 = 12;
 
-    let zh = setup();
+    let mut map = BytesTrieMap::new();
+    let zh = map.zipper_head();
 
     let mut buildz = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&[0]) };
     buildz.graft_map(BytesTrieMap::range::<true, u64>(0, K, 1, ()));
     drop(buildz);
     let mut dz = unsafe { zh.read_zipper_at_path_unchecked(&[0]) };
 
-    let mut dispatches = 0;
-    let mut chunks = 0;
-    let t0 = Instant::now();
-    let mut run = 0;
-    let mut units = vec![];
-    let mut handles = vec![];
-    homo(TC, &mut dz, &mut |cs, z: &mut ReadZipperUntracked<()>| {
-        let cutoff = cs.div_ceil(TC);
-        z.descend_first_byte();
-        loop {
-            chunks += 1;
-            run += 1;
+    std::thread::scope(|scope| {
 
-            // println!("p {:?} c {}", z.path(), z.child_mask().iter().fold(0, |t, x| t + x.count_ones()))
-            let mut work_input = unsafe { zh.read_zipper_at_path_unchecked(z.origin_path().unwrap()) };
-            let mut opath = vec![1];
-            opath.extend_from_slice(z.path());
-            // println!("created zpath={:?} ({}) opath={opath:?} for {}", z.path(), z.val_count(), dispatches + 1);
-            let mut work_output = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&opath[..]) };
+        let mut dispatches = 0;
+        let mut chunks = 0;
+        let t0 = Instant::now();
+        let mut run = 0;
+        let mut units = vec![];
+        let mut handles = vec![];
+        homo(TC, &mut dz, &mut |cs, z: &mut ReadZipperUntracked<()>| {
+            let cutoff = cs.div_ceil(TC);
+            z.descend_first_byte();
+            loop {
+                chunks += 1;
+                run += 1;
 
-            units.push((work_input, work_output));
+                // println!("p {:?} c {}", z.path(), z.child_mask().iter().fold(0, |t, x| t + x.count_ones()))
+                let work_input = unsafe { zh.read_zipper_at_path_unchecked(z.origin_path().unwrap()) };
+                let mut opath = vec![1];
+                opath.extend_from_slice(z.path());
+                // println!("created zpath={:?} ({}) opath={opath:?} for {}", z.path(), z.val_count(), dispatches + 1);
+                let work_output = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&opath[..]) };
 
-            if run >= cutoff {
-                // dispatch and clear
-                let mut thread_units = std::mem::take(&mut units);
-                run = 0;
-                dispatches += 1;
-                println!("dispatched {} up to {} ({})", dispatches, chunks, thread_units.len());
-                handles.push(std::thread::spawn(move || {
-                    // println!("thread {} working", dispatches);
-                    // work_output.set_value(());
-                    for (work_input, work_output) in thread_units.iter_mut() {
-                        // work_output.graft(work_input);
-                        // println!("thread {} processing origin={:?} (queued: {})", dispatches, work_input.origin_path(), work_input.val_count());
-                        loop {
-                            if work_input.to_next_val().is_none() { break }
-                            // println!("tp {:?}", work_input.path());
-                            let mut buffer = [0; 8];
-                            for (s, t) in work_input.path().iter().rev().zip(buffer.iter_mut().rev()) {
-                                *t = *s;
+                units.push((work_input, work_output));
+
+                if run >= cutoff {
+                    // dispatch and clear
+                    let mut thread_units = std::mem::take(&mut units);
+                    run = 0;
+                    dispatches += 1;
+                    println!("dispatched {} up to {} ({})", dispatches, chunks, thread_units.len());
+                    handles.push(scope.spawn(move || {
+                        // println!("thread {} working", dispatches);
+                        // work_output.set_value(());
+                        for (work_input, work_output) in thread_units.iter_mut() {
+                            // work_output.graft(work_input);
+                            // println!("thread {} processing origin={:?} (queued: {})", dispatches, work_input.origin_path(), work_input.val_count());
+                            loop {
+                                if work_input.to_next_val().is_none() { break }
+                                // println!("tp {:?}", work_input.path());
+                                let mut buffer = [0; 8];
+                                for (s, t) in work_input.path().iter().rev().zip(buffer.iter_mut().rev()) {
+                                    *t = *s;
+                                }
+                                let v = u64::from_be_bytes(buffer);
+                                work_output.descend_to(work_input.path());
+                                let vr = (v as f64).sqrt() as u64;
+                                // println!("calculated f({v}) = {vr}");
+                                work_output.descend_to(&vr.to_be_bytes()[..]);
+                                work_output.set_value(());
+                                work_output.reset();
                             }
-                            let v = u64::from_be_bytes(buffer);
-                            work_output.descend_to(work_input.path());
-                            let vr = (v as f64).sqrt() as u64;
-                            // println!("calculated f({v}) = {vr}");
-                            work_output.descend_to(&vr.to_be_bytes()[..]);
-                            work_output.set_value(());
-                            work_output.reset();
                         }
-                    }
-                    // println!("thread {} done working", dispatches);
-                }));
-            }
-
-            if !z.to_sibling(true) { break }
-        }
-        z.ascend_byte();
-    });
-    if run > 0 {
-        let mut thread_units = std::mem::take(&mut units);
-        run = 0;
-        dispatches += 1;
-        println!("dispatched {} up to {} ({})", dispatches, chunks, thread_units.len());
-        handles.push(std::thread::spawn(move || {
-            // work_output.set_value(());
-            for (work_input, work_output) in thread_units.iter_mut() {
-                // work_output.graft(work_input);
-                loop {
-                    if work_input.to_next_val().is_none() { break }
-                    // println!("tp {:?}", work_input.path());
-                    let mut buffer = [0; 8];
-                    for (s, t) in work_input.path().iter().rev().zip(buffer.iter_mut().rev()) {
-                        *t = *s;
-                    }
-                    let v = u64::from_be_bytes(buffer);
-                    work_output.descend_to(work_input.path());
-                    let vr = (v as f64).sqrt() as u64;
-                    // println!("calculated f({v}) = {vr}");
-                    work_output.descend_to(&vr.to_be_bytes()[..]);
-                    work_output.set_value(());
-                    work_output.reset();
+                        // println!("thread {} done working", dispatches);
+                    }));
                 }
+
+                if !z.to_sibling(true) { break }
             }
-        }));
-    }
-    drop(dz);
-    println!("delegating {chunks} chunks took {} micros", t0.elapsed().as_micros());
+            z.ascend_byte();
+        });
+        if run > 0 {
+            let mut thread_units = std::mem::take(&mut units);
+            dispatches += 1;
+            println!("dispatched {} up to {} ({})", dispatches, chunks, thread_units.len());
+            handles.push(scope.spawn(move || {
+                // work_output.set_value(());
+                for (work_input, work_output) in thread_units.iter_mut() {
+                    // work_output.graft(work_input);
+                    loop {
+                        if work_input.to_next_val().is_none() { break }
+                        // println!("tp {:?}", work_input.path());
+                        let mut buffer = [0; 8];
+                        for (s, t) in work_input.path().iter().rev().zip(buffer.iter_mut().rev()) {
+                            *t = *s;
+                        }
+                        let v = u64::from_be_bytes(buffer);
+                        work_output.descend_to(work_input.path());
+                        let vr = (v as f64).sqrt() as u64;
+                        // println!("calculated f({v}) = {vr}");
+                        work_output.descend_to(&vr.to_be_bytes()[..]);
+                        work_output.set_value(());
+                        work_output.reset();
+                    }
+                }
+            }));
+        }
+        drop(dz);
+        println!("delegating {chunks} chunks took {} micros", t0.elapsed().as_micros());
 
-    let t1 = Instant::now();
-    for handle in handles { handle.join().unwrap() }
-    // sleep(Duration::new(1, 0));
-    println!("processing took {} micros", t1.elapsed().as_micros());
+        let t1 = Instant::now();
+        for handle in handles { handle.join().unwrap() }
+        // sleep(Duration::new(1, 0));
+        println!("processing took {} micros", t1.elapsed().as_micros());
 
-    let mut rz = unsafe { zh.read_zipper_at_path_unchecked(&[]) };
+    let rz = unsafe { zh.read_zipper_at_path_unchecked(&[]) };
     println!("result count: {}", rz.val_count());
     // while let Some(_) = rz.to_next_val() {
     //     println!("o {:?}", rz.path());
     // }
+    });
 }
 
+#[allow(unused)]
 fn task_parallel_map() {
     const TC: u32 = 32;
     let pool = ThreadPoolBuilder::new().num_threads(TC as usize).build().unwrap();
-    static mut counter: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
+    static COUNTER: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
 
-    let zh = setup();
+    let mut map = BytesTrieMap::new();
+    let zh = map.zipper_head();
 
     let mut buildz = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&[0]) };
     buildz.graft_map(BytesTrieMap::range::<true, u64>(0, K, 1, ()));
     drop(buildz);
-    let mut dz = unsafe { zh.read_zipper_at_path_unchecked(&[0]) };
 
+    let mut work_zippers = vec![];
+    let mut dz = unsafe { zh.read_zipper_at_path_unchecked(&[0]) };
     let mut chunks = 0;
     let t0 = Instant::now();
     homo(TC, &mut dz, &mut |_, z: &mut ReadZipperUntracked<()>| {
@@ -176,13 +171,25 @@ fn task_parallel_map() {
         loop {
             chunks += 1;
             // println!("p {:?} c {}", z.path(), z.child_mask().iter().fold(0, |t, x| t + x.count_ones()))
-            let mut work_input = unsafe { zh.read_zipper_at_path_unchecked(z.origin_path().unwrap()) };
+            let work_input = unsafe { zh.read_zipper_at_path_unchecked(z.origin_path().unwrap()) };
             let mut opath = vec![1];
             opath.extend_from_slice(&z.path()[..]);
             // println!("dispatched zpath={:?} ({}) opath={opath:?}", z.path(), z.val_count());
-            let mut work_output = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&opath[..]) };
-            unsafe { counter.fetch_add(1, Ordering::Relaxed) };
-            pool.spawn(move || {
+            let work_output = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&opath[..]) };
+            unsafe { COUNTER.fetch_add(1, Ordering::Relaxed) };
+
+            work_zippers.push((work_input, work_output));
+
+            if !z.to_sibling(true) { break }
+        }
+        z.ascend_byte();
+    });
+    drop(dz);
+
+    let mut t1 = Instant::now();
+    pool.scope(|scope| {
+        for (mut work_input, mut work_output) in work_zippers {
+            scope.spawn(move |_| {
                 // work_output.set_value(());
                 loop {
                     if work_input.to_next_val().is_none() { break }
@@ -199,31 +206,29 @@ fn task_parallel_map() {
                     work_output.set_value(());
                     work_output.reset();
                 }
-                unsafe { counter.fetch_sub(1, Ordering::Relaxed) };
+                unsafe { COUNTER.fetch_sub(1, Ordering::Relaxed) };
             });
-
-            if !z.to_sibling(true) { break }
         }
-        z.ascend_byte();
-    });
-    drop(dz);
-    println!("delegating {chunks} chunks took {} micros", t0.elapsed().as_micros());
+        println!("delegating {chunks} chunks took {} micros", t0.elapsed().as_micros());
 
-    let t1 = Instant::now();
-    while unsafe { counter.load(Ordering::Acquire) > 0 } {}
-    // sleep(Duration::new(1, 0));
+        t1 = Instant::now();
+    });
+
+    while unsafe { COUNTER.load(Ordering::Acquire) > 0 } {}
     println!("processing took {} micros", t1.elapsed().as_micros());
 
-    let mut rz = unsafe { zh.read_zipper_at_path_unchecked(&[]) };
+    let rz = unsafe { zh.read_zipper_at_path_unchecked(&[]) };
     println!("result count: {}", rz.val_count());
     // while let Some(_) = rz.to_next_val() {
     //     println!("o {:?}", rz.path());
     // }
 }
 
+#[allow(unused)]
 fn sequential_map() {
 
-    let zh = setup();
+    let mut map = BytesTrieMap::new();
+    let zh = map.zipper_head();
 
     let mut buildz = unsafe { zh.write_zipper_at_exclusive_path_unchecked(&[0]) };
     buildz.graft_map(BytesTrieMap::range::<true, u64>(0, K, 1, ()));
@@ -250,7 +255,7 @@ fn sequential_map() {
     drop(oz);
     println!("processing took {} micros", t0.elapsed().as_micros());
 
-    let mut rz = unsafe { zh.read_zipper_at_path_unchecked(&[]) };
+    let rz = unsafe { zh.read_zipper_at_path_unchecked(&[]) };
     println!("result count: {}", rz.val_count());
     // while let Some(_) = rz.to_next_val() {
     //     println!("o {:?}", rz.path());

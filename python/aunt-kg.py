20a11ec2dc18d038c8eb036f57f65603d4a801d2
Helder S Ribeiro <helder@agentofuser.com>
2025-07-03 16:33:29 -0400

    Add mother, sister, aunt relations

--- python/aunt-kg.py
+++ python/aunt-kg.py
@@ -18,6 +18,19 @@ def preprocessing(server, datasets=DATASETS):
                     downloaded = src.download("(Individuals $i (Fullname $name))", "$name")
                     print("names", dataset, downloaded.data)
 
+                # Generate isIdDifferent relations so that sister relations aren't reflective
+                ids_string = scope.download("(src (Individuals $i (Id $id)))", "$id").data
+                ids = [line.strip()[1:-1] for line in ids_string.strip().splitlines() if line.strip()]
+
+                entries = []
+                for id0 in ids:
+                    for id1 in ids:
+                        if id0 != id1:
+                            entry = f'(simple (isIdDifferent "{id0}" "{id1}"))'
+                            entries.append(entry)
+                to_upload = "\n".join(entries)
+                scope.upload_(to_upload)
+
                 scope.transform(("(src (Individuals $i (Id $id)))", "(src (Individuals $i (Fullname $name)))"), ("(simple (hasName $id $name))", "(simple (hasId $name $id))")).block()
 
                 scope.transform(("(src (Individuals $i (Id $id)))", "(src (Individuals $i (Sex \"M\")))"), ("(simple (male $id))",)).block()
@@ -29,6 +42,16 @@ def preprocessing(server, datasets=DATASETS):
                 scope.transform(("(src (Relations $r (Husband $id)))", "(src (Relations $r (Children $cid)))"), ("(simple (parent $id $cid))",))\
                     .block()
                 scope.transform(("(src (Relations $r (Wife $id)))", "(src (Relations $r (Children $cid)))"), ("(simple (parent $id $cid))",)).block()
+
+                scope.transform(("(simple (parent $pid $cid))", "(simple (female $pid))",), ("(simple (mother $pid $cid))",),).block()
+                scope.transform(("(simple (mother $pid $cid))", "(simple (hasName $pid $name0))", "(simple (hasName $cid $name1))",), ("(simple (motherByName $name0 $name1))",),).block()
+
+                scope.transform(("(simple (parent $pid $cid0))", "(simple (parent $pid $cid1))", "(simple (isIdDifferent $cid0 $cid1))", "(simple (female $cid0))",), ("(simple (sister $cid0 $cid1))",),).block()
+                scope.transform(("(simple (sister $cid0 $cid1))", "(simple (hasName $cid0 $name0))", "(simple (hasName $cid1 $name1))",), ("(simple (sisterByName $name0 $name1))",),).block()
+
+                scope.transform(("(simple (parent $pid $cid))", "(simple (sister $aid $pid))",), ("(simple (aunt $aid $cid))",),).block()
+                scope.transform(("(simple (aunt $aid $cid))", "(simple (hasName $aid $name0))", "(simple (hasName $cid $name1))",), ("(simple (auntByName $name0 $name1))",),).block()
+
 
     ins.sexpr_export("($dataset (simple $x))", "($dataset $x)", "file://" + __file__.rpartition("/")[0] + "/simple_all.metta")
 

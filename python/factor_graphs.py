e1fcf5941e1084d860005be08b81054267dfc7a8
Adam Vandervorst <adam.vandervorst@gmail.com>
2025-07-29 12:09:16 +0200

    Type and add marginal aggregation to round definition

--- python/factor_graphs.py
+++ python/factor_graphs.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 from random import random
+from typing import TypeAlias, Callable, Optional, Iterable
 
+Formula: TypeAlias = str
+STV: TypeAlias = tuple[float, float]
+Comb: TypeAlias = tuple[Formula, STV]
 
 class Variable:
     all: 'list[Variable]' = []
-    def __init__(self, name, formula, stv):
+    def __init__(self, name: str, formula: Formula, stv: STV):
         self.name = name
         self.formula = formula
         self.stv = stv
@@ -16,7 +20,7 @@ class Variable:
 
 class Factor:
     all: 'list[Factor]' = []
-    def __init__(self, name, combined_potential):
+    def __init__(self, name: str, combined_potential: Callable[[Comb], Optional[Comb]]):
         self.name = name
         self.combined_potential = combined_potential
         self.to_variables = []
@@ -29,67 +33,61 @@ def link(v: Variable, f: Factor):
     v.to_factors.append(f)
     f.to_variables.append(v)
 
-def mul_stv(x, y):
+def mul_stv(x: STV, y: STV) -> STV:
     (s1, c1) = x
     (s2, c2) = y
     return s1*s2 , 1. - (1. - c1)*(1. - c2)
 
-def add_stv(x, y):
+def add_stv(x: STV, y: STV) -> STV:
     (s1, c1) = x
     (s2, c2) = y
     return s1 + s2 - s1*s2, c1 + c2 - c1*c2
 
-def add_all_stv(xs):
+def add_all_stv(xs: Iterable[STV]) -> STV:
     z = (0., 0.)
     for x in xs: z = add_stv(z, x)
     return z
 
-def logic_join(x, y):
+def logic_join(x: Formula, y: Formula) -> Formula:
     return f"({x} | {y})"
 
-def logic_meet(x, y):
+def logic_meet(x: Formula, y: Formula) -> Formula:
     return f"({x} & {y})"
 
-BOTTOM = ("false", (0.,0.))
-UNIT = ("true", (0.,0.))
+BOTTOM: Comb = ("false", (0.,0.))
+UNIT: Comb = ("true", (1.,0.))
 
-def join(x, y):
+def join(x: Comb, y: Comb) -> Comb:
     ɸ1, ɑ1 = x
     ɸ2, ɑ2 = y
     return logic_join(ɸ1, ɸ2), add_stv(ɑ1, ɑ2)
 
-def join_all(xs):
+def join_all(xs: Iterable[Comb]) -> Comb:
     z = BOTTOM
     for x in xs: z = join(x, z)
     return z
 
-def mul(x, y):
+def mul(x: Comb, y: Comb) -> Comb:
     ɸ1, ɑ1 = x
     ɸ2, ɑ2 = y
     return logic_meet(ɸ1, ɸ2), mul_stv(ɑ1, ɑ2)
 
-def mul_all(xs):
+def mul_all(xs: Iterable[Comb]) -> Comb:
     z = UNIT
     for x in xs: z = mul(x, z)
     return z
 
-def random_stv():
+def random_stv() -> STV:
     return random(), random()
 
-def DeductionSTV(stv):
+def DeductionSTV(stv: STV) -> STV:
     (s, c) = stv
     return .8*s, .8*c
 
-def phi_ded(x):
+def phi_ded(x: Comb) -> Optional[Comb]:
     ɸ, ɑ = x
     if "Dog" in ɸ: # a heavy stand-in...
         return ɸ.replace("Dog", "Mammal"), DeductionSTV(ɑ)
-
-def quantify(vs, f):
-    print("quantify", {v.name for v in vs}, f)
-    # This is closer to what I'd have expected, but produces the wrong result
-    # return "(" + " | ".join(f.replace("x", v.formula) for v in vs) + ")"  # a heavy stand-in...
-    return f
 
 def silly():
     vDog = Variable("vDog", "Dog(Lassie)", (0.9, 0.9))
@@ -98,8 +96,8 @@ def silly():
     link(vDog, fDed)
     link(vMammal, fDed)
 
-    varToFac = defaultdict(dict)
-    facToVar = defaultdict(dict)
+    varToFac: defaultdict[Variable, dict[Factor, Comb]] = defaultdict(dict)
+    facToVar: defaultdict[Factor, dict[Variable, Comb]] = defaultdict(dict)
     def round():
         nonlocal varToFac
         for v in Variable.all:
@@ -119,13 +117,21 @@ def silly():
                     print(f"skipping {f.name}->{v.name}")
                     continue
                 (new_formula, new_stv) = m_potential
-                # I expected add_all_stv(v_.stv for v_ in other_v) instead of new_stv
-                new_v = (quantify(other_v, new_formula), new_stv)
-                facToVar[f][v] = new_v
+                new_v = ("(" + " | ".join(new_formula.replace("x", v_.formula) for v_ in other_v) + ")",
+                         add_all_stv(v_.stv for v_ in other_v))
+                print("new v", new_v)
+                print("m_potential", m_potential)
+                # throwing away new_v at the moment!
+                # neither mul(new_v, m_potential) nor join(new_v, m_potential) makes sense here I think
+                facToVar[f][v] = m_potential
                 print(f"m_{{{f.name}->{v.name}}} = {new_v}")
+        for v in Variable.all:
+            ɸ, ɑ = marginal(v)
+            v.formula = ɸ
+            v.stv = ɑ
 
-    def marginal(v):
-        return join_all(facToVar[f][v] for f in v.to_factors)
+    def marginal(v: Variable) -> Comb:
+        return join_all(facToVar[f][v] for f in v.to_factors if v in facToVar[f])
 
     round()
     print("marginal", marginal(vMammal))

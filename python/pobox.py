4222c7f308a575a6ac41ddbcc8ae3f95e33e1b14
Adam Vandervorst <adam.vandervorst@gmail.com>
2025-09-05 12:10:56 +0200

    Add Evgeny's paths import and property export example

---
+++ python/pobox.py
@@ -0,0 +1,17 @@
+from client import MORK, ManagedMORK
+
+def show_all(ins):
+    print(ins.download_().data)
+
+def get_address(ins):
+    print("result: ", ins.download("(Head (Source (Corporation (Address $addr))))", "(Got Addr $addr)").data)
+
+def _main():
+    with ManagedMORK.connect(binary_path="../target/release/mork_server").and_terminate() as server:
+        server.clear().block() # for the example, make sure we have a clear workspace
+        server.paths_import_("file://" + __file__.rpartition("/")[0] + "/resources/pobox.paths")
+        show_all(server)
+        get_address(server)
+
+if __name__ == '__main__':
+    _main()

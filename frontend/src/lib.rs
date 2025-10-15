a567171581730690249ecf54cc0b4dbdd2c80cb5
Adam Vandervorst <adam.vandervorst@gmail.com>
2025-10-15 02:10:13 +0200

    Recover tests

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -5,3 +5,16 @@
 
 pub mod bytestring_parser;
 pub mod json_parser;
+
+
+/*
+fn partial_reconstruct_numeric_json() {
+    let json_input = r#"{"pos": 42, "neg": -100, "pi": 3.1415926, "winter": -20.5, "google": 1e+100}"#;
+    let json_output = r#"{"pos": 42, "neg": -100, "pi": 31415926e-7, "winter": -205e-1, "google": 1e100}"#;
+
+    let mut p = Parser::new(json_input);
+    let mut wt = WriteTranscriber{ w: Vec::<u8>::new() };
+    p.parse(&mut wt).unwrap();
+    assert_eq!(json_output, String::from_utf8(wt.w).unwrap());
+}
+*/
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,14 +1,24 @@
-use std::collections::{BTreeSet, HashSet};
-// use std::future::Future;
-// use std::task::Poll;
-use std::time::Instant;
-use pathmap::PathMap;
-use pathmap::zipper::{Zipper, ZipperAbsolutePath, ZipperIteration, ZipperMoving};
-use mork_frontend::bytestring_parser::Parser;
 use mork::{expr, prefix, sexpr};
 use mork::space::{transitions, unifications, writes, Space};
+use mork_frontend::bytestring_parser::Parser;
 use mork_expr::{item_byte, Tag};
+use pathmap::PathMap;
+use pathmap::zipper::{Zipper, ZipperAbsolutePath, ZipperIteration, ZipperMoving};
+use std::collections::{BTreeSet, HashSet};
+use std::time::Instant;
+use std::ffi::OsStr;
+use std::ffi::OsString;
+use std::hash::{Hash, Hasher};
+use std::ops::Add;
+// use std::future::Future;
+// use std::task::Poll;
 use itertools::Itertools;
+use base64::Engine;
+use serde::{Serialize, Deserialize};
+use clap::{Args, Parser as CLAParser, Subcommand, ValueEnum};
+use clap::builder::TypedValueParser;
+
+
 /*fn main() {
     let mut s = Space::new();
     let t0 = Instant::now();
@@ -177,6 +187,10 @@ query BRCA2
  151956 atoms
  */
 
+fn set_from_newlines(input : &str) -> BTreeSet<&str> {
+    BTreeSet::from_iter(input.split('\n').filter(|s| !s.is_empty()))
+}
+
 fn peano(x: usize) -> String {
     if x == 0 { "Z".to_string() }
     else { format!("(S {})", peano(x - 1)) }
@@ -243,7 +257,7 @@ fn process_calculus_bench(steps: usize, x: usize, y: usize) {
     let t0 = Instant::now();
     let mcalc_steps = s.metta_calculus(1000000000000000); // big number to show the MM2 inference control working
     let elapsed = t0.elapsed();
-    
+
     let mut v = vec![];
     // s.dump_all_sexpr(&mut v).unwrap();
     // We're only interested in the petri dish (not the state of exec), and specifically everything that was outputted `!` to `result`
@@ -952,6 +966,29 @@ fn logic_query() {
     let steps = s.metta_calculus(1000000000000000);
 
     assert_eq!(s.btm.val_count(), 79);
+}
+
+fn bench_logic_query() {
+    use std::io::Read;
+    let mut s = Space::new();
+
+    let mut expr_buf = vec![];
+    std::fs::File::open("resources/big.metta").unwrap().read_to_end(&mut expr_buf).unwrap();
+    s.add_all_sexpr(&expr_buf[..]).unwrap();
+
+    let mut t0 = Instant::now();
+    s.add_all_sexpr(b"(exec 0 (, (axiom $x) (axiom $x)) (, (combined $x)))").unwrap();
+    s.metta_calculus(1);
+    println!("combined elapsed {} ms size {}", t0.elapsed().as_millis(), s.btm.val_count());
+
+    let mut t1 = Instant::now();
+    s.add_all_sexpr(b"(exec 0 (, (axiom (= $lhs $rhs)) (axiom (= $rhs $lhs))) (, (reversed $lhs $rhs)))").unwrap();
+    s.metta_calculus(1);
+    println!("reversed elapsed {} ms size {}", t1.elapsed().as_millis(), s.btm.val_count());
+    
+    // yikes, this is much slower than the old bidirectional transition in `server`?
+    // combined elapsed 236156 ms size 1677208
+    // reversed elapsed 435670 ms size 3348972
 }
 
 fn bc0() {
@@ -2369,58 +2406,28 @@ fn mm2_bc_v3() {
     }
 }
 
-/*
-fn set_from_newlines(input : &str) -> std::collections::BTreeSet<&str> {
-    let mut set = std::collections::BTreeSet::new();
-    for each in input.split('\n').filter(|s| !s.is_empty()) {
-        set.insert(each);
-    }
-    set
-}
-
-#[test]
-fn prefix_parse_sexpr() {
-    let input = "((nested and) (singleton))\n(foo bar)\n(1 \"test\" 2)\n";
-    let mut s = Space::new();
-    assert_eq!(s.add_sexpr(input.as_bytes(), expr!(s, "$"), expr!(s, "[2] my [2] prefix _1")).unwrap(), 3);
-    let mut res = Vec::<u8>::new();
-    s.dump_sexpr(expr!(s, "[2] my [2] prefix $"), expr!(s, "_1"), &mut res).unwrap();
-
-    // the order changed in the test for some reason so we need to use sets to not be concerened by this
-    let out = String::from_utf8(res).unwrap();
-    assert_eq!(set_from_newlines(input), set_from_newlines(&out));
-}
-
-#[test]
 fn parse_csv() {
-    let csv_input = "0,123,foo\n1,321,bar\n";
-    let reconstruction = "(0 123 foo)\n(1 321 bar)\n";
+    let csv_input = "10,123,foo\n11,321,bar\n";
+    let reconstruction = "(0 10 123 foo)\n(1 11 321 bar)\n";
     let mut s = Space::new();
     assert_eq!(s.load_csv(csv_input.as_bytes(), expr!(s, "$"), expr!(s, "_1"), b',').unwrap(), 2);
     let mut res = Vec::<u8>::new();
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"),&mut res).unwrap();
+    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"),&mut res);
     assert_eq!(reconstruction, String::from_utf8(res).unwrap());
 }
 
-#[test]
-fn reconstruct_json() {
+fn parse_json() {
     let json_input = r#"{"first_name": "John", "last_name": "Smith", "is_alive": true, "age": 27, "address": {"street_address": "21 2nd Street", "city": "New York", "state": "NY", "postal_code": "10021-3100"}, "phone_numbers": [{"type": "home", "number": "212 555-1234"}, {"type": "office", "number": "646 555-4567"}], "children": ["Catherine", "Thomas", "Trevor"], "spouse": null}"#;
 
-    let mut p = Parser::new(json_input);
-    let mut wt = WriteTranscriber{ w: Vec::<u8>::new() };
-    p.parse(&mut wt).unwrap();
-    assert_eq!(json_input, String::from_utf8(wt.w).unwrap());
-}
+    let mut s = Space::new();
+    s.load_json(json_input.as_bytes());
 
-#[test]
-fn partial_reconstruct_numeric_json() {
-    let json_input = r#"{"pos": 42, "neg": -100, "pi": 3.1415926, "winter": -20.5, "google": 1e+100}"#;
-    let json_output = r#"{"pos": 42, "neg": -100, "pi": 31415926e-7, "winter": -205e-1, "google": 1e100}"#;
-
-    let mut p = Parser::new(json_input);
-    let mut wt = WriteTranscriber{ w: Vec::<u8>::new() };
-    p.parse(&mut wt).unwrap();
-    assert_eq!(json_output, String::from_utf8(wt.w).unwrap());
+    let mut v = vec![];
+    s.dump_all_sexpr(&mut v).unwrap();
+    let res = String::from_utf8(v).unwrap();
+    
+    println!("{}", res);
+    assert_eq!(set_from_newlines(SEXPRS0), set_from_newlines(res.as_str()));
 }
 
 const SEXPRS0: &str = r#"(first_name John)
@@ -2441,340 +2448,6 @@ const SEXPRS0: &str = r#"(first_name John)
 (spouse null)
 "#;
 
-#[test]
-fn parse_json() {
-    let json_input = r#"{
-"first_name": "John",
-"last_name": "Smith",
-"is_alive": true,
-"age": 27,
-"address": {
-"street_address": "21 2nd Street",
-"city": "New York",
-"state": "NY",
-"postal_code": "10021-3100"},
-"phone_numbers": [
-{"type": "home", "number": "212 555-1234"},
-{"type": "office", "number": "646 555-4567"}],
-"children": ["Catherine", "Thomas", "Trevor"],
-"spouse": null}"#;
-
-    let mut s = Space::new();
-
-    assert_eq!(16, s.load_json(json_input.as_bytes()).unwrap());
-
-    let mut res = Vec::<u8>::new();
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"), &mut res).unwrap();
-
-    let out = String::from_utf8(res).unwrap();
-    assert_eq!(set_from_newlines(SEXPRS0), set_from_newlines(&out));
-}
-
-#[test]
-fn query_simple() {
-    let mut s = Space::new();
-    assert_eq!(16, s.add_sexpr( SEXPRS0.as_bytes(), expr!(s, "$"), expr!(s, "_1"),).unwrap());
-
-    let mut i = 0;
-    s.query(expr!(s, "[2] children [2] $ $"), |_, e| {
-        match i {
-            0 => { assert_eq!(sexpr!(s, e), "(children (0 Catherine))") }
-            1 => { assert_eq!(sexpr!(s, e), "(children (1 Thomas))") }
-            2 => { assert_eq!(sexpr!(s, e), "(children (2 Trevor))") }
-            _ => { assert!(false) }
-        }
-        i += 1;
-    });
-}
-
-#[test]
-fn transform_simple() {
-    let mut s = Space::new();
-    assert_eq!(16, s.add_sexpr(SEXPRS0.as_bytes(), expr!(s, "$"), expr!(s, "_1"),).unwrap());
-
-    s.transform(expr!(s, "[2] children [2] $ $"), expr!(s, "[2] child_results _2"));
-    let mut i = 0;
-    s.query(expr!(s, "[2] child_results $x"), |_, e| {
-        match i {
-            0 => { assert_eq!(sexpr!(s, e), "(child_results Catherine)") }
-            1 => { assert_eq!(sexpr!(s, e), "(child_results Thomas)") }
-            2 => { assert_eq!(sexpr!(s, e), "(child_results Trevor)") }
-            _ => { assert!(false) }
-        }
-        i += 1;
-    });
-}
-
-#[test]
-fn transform_multi() {
-    let mut s = Space::new();
-    let mut file = File::open("/home/adam/Projects/MORK/benchmarks/aunt-kg/resources/simpsons.metta").unwrap();
-    let mut fileb = vec![]; file.read_to_end(&mut fileb);
-    s.add_sexpr(fileb.as_slice(), expr!(s, "$"), expr!(s, "_1")).unwrap();
-
-    s.transform_multi(&[expr!(s, "[3] Individuals $ [2] Id $"),
-                               expr!(s, "[3] Individuals _1 [2] Fullname $")],
-                      expr!(s, "[3] hasName _2 _3"));
-
-    // let mut res = Vec::<u8>::new();
-    // s.dump(&mut res).unwrap();
-    // println!("{}", String::from_utf8(res).unwrap());
-}
-
-const LOGICSEXPR0: &str = r#"(axiom (= (L $x $y $z) (R $x $y $z)))
-(axiom (= (L 1 $x $y) (R 1 $x $y)))
-(axiom (= (R $x (L $x $y $z) $w) $x))
-(axiom (= (R $x (R $x $y $z) $w) $x))
-(axiom (= (R $x (L $x $y $z) $x) (L $x (L $x $y $z) $x)))
-(axiom (= (L $x $y (\ $y $z)) (L $x $y $z)))
-(axiom (= (L $x $y (* $z $y)) (L $x $y $z)))
-(axiom (= (L $x $y (\ $z 1)) (L $x $z $y)))
-(axiom (= (L $x $y (\ $z $y)) (L $x $z $y)))
-(axiom (= (L $x 1 (\ $y 1)) (L $x $y 1)))
-(axiom (= (T $x (L $x $y $z)) $x))
-(axiom (= (T $x (R $x $y $z)) $x))
-(axiom (= (T $x (a $x $y $z)) $x))
-(axiom (= (T $x (\ (a $x $y $z) $w)) (T $x $w)))
-(axiom (= (T $x (* $y $y)) (T $x (\ (a $x $z $w) (* $y $y)))))
-(axiom (= (R (/ 1 $x) $x (\ $x 1)) (\ $x 1)))
-(axiom (= (\ $x 1) (/ 1 (L $x $x (\ $x 1)))))
-(axiom (= (L $x $x $x) (* (K $x (\ $x 1)) $x)))"#;
-
-#[test]
-fn subsumption() {
-    let mut s = Space::new();
-    s.add_sexpr(LOGICSEXPR0.as_bytes(), expr!(s, "$"), expr!(s, "_1")).unwrap();
-
-    // s.transform(expr!(s, "[2] axiom [3] = _2 _1"), expr!(s, "[2] flip [3] = $ $"));
-    s.transform(expr!(s, "[2] axiom [3] = $ $"), expr!(s, "[2] flip [3] = _2 _1"));
-    let mut c_in = 0; s.query(expr!(s, "[2] axiom [3] = $ $"), |_,e| c_in += 1);
-    let mut c_out = 0; s.query(expr!(s, "[2] flip [3] = $ $"), |_,e| c_out += 1);
-    assert_eq!(c_in, c_out);
-
-    let mut res = Vec::<u8>::new();
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"), &mut res).unwrap();
-    println!("{}", String::from_utf8(res).unwrap());
-}
-
-#[test]
-fn big_subsumption() {
-    let mut s = Space::new();
-    let mut file = std::fs::File::open("/home/adam/Projects/MORK/benchmarks/logic-query/resources/big.metta")
-      .expect("Should have been able to read the file");
-    let mut buf = vec![];
-    file.read_to_end(&mut buf).unwrap();
-    s.add_sexpr(&buf[..], expr!(s, "$"), expr!(s, "_1")).unwrap();
-
-    // expr!(s, "[2] flip [3] \"=\" _2 _1")
-    // s.transform(expr!(s, "[2] assert [3] forall $ $"), expr!(s, "axiom _2"));
-    // s.transform(expr!(s, "[2] axiom [3] = $ $"), expr!(s, "[2] flip [3] = _2 _1"));
-    // s.query(expr!(s, "[2] axiom [3] = $ $"), |e| { println!("> {}", sexpr!(s, e)) });
-    let t0 = Instant::now();
-    let mut k = 0;
-    s.query(expr!(s, "$x"), |_,e| {
-        k += 1;
-        std::hint::black_box(e);
-        // println!("> {}", sexpr!(s, e))
-    });
-    println!("iterating all ({}) took {} microseconds", k, t0.elapsed().as_micros());
-
-
-
-    // let mut res = Vec::<u8>::new();
-    // s.dump(&mut res).unwrap();
-    // println!("{}", String::from_utf8(res).unwrap());
-}
-
-#[test]
-fn transform_multi_multi_no_match() {
-    let mut s = Space::new();
-
-    s.transform_multi_multi(&[expr!(s, "a")], &[expr!(s, "c")]);
-
-    let mut writer = Vec::new();
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"), &mut writer);
-
-    let out = unsafe {
-        core::mem::transmute::<_,String>(writer)
-    };
-
-    println!("{}", out);
-
-    core::assert_ne!(&out, "c\n");
-}
-
-
-#[test]
-fn transform_multi_multi_ignoring_second_template() {
-    let mut s = Space::new();
-            const SPACE_EXPRS: &str =
-    concat!
-    ( "\n(val a b)"
-    );
-
-    s.add_sexpr(SPACE_EXPRS.as_bytes(), expr!(s, "$"), expr!(s, "_1")).unwrap();
-
-    s.transform_multi_multi(&[expr!(s, "[3] val $ $")], &[expr!(s, "_1"), expr!(s, "_2")]);
-
-    let mut writer = Vec::new();
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"), &mut writer);
-
-    let out = unsafe {
-        core::mem::transmute::<_,String>(writer)
-    };
-
-    println!("{}", out);
-
-    let vals = ["a","b","(val a b)"];
-    for each in vals {
-        assert!(out.lines().any(|i| i == each))
-    }
-}
-
-#[test]
-fn metta_calculus_test0() {
-    let mut s = Space::new();
-    // (exec PC0 (, (? $channel $payload $body)
-    //              (! $channel $payload)
-    //              (exec PC0 $p $t))
-    //           (, $body (exec PC0 $p $t)))
-
-    // const SPACE_EXPRS: &str = r#"
-    // (exec PC0 (, (? $channel $payload $body) (! $channel $payload) (exec PC0 $p $t)) (, $body (exec PC0 $p $t)))
-    // (exec PC1 (, (| $lprocess $rprocess) (exec PC1 $p $t)) (, $lprocess $rprocess (exec PC1 $p $t)))
-
-    // (? (add $ret) ((S $x) $y) (? (add $z) ($x $y) (! $ret (S $z)) ) )
-    // (? (add $ret) (Z $y) (! $ret $y))
-
-    // (! (add result) ((S Z) (S Z)))
-    // "#;
-
-    const SPACE_EXPRS: &str =
-    concat!
-    ( ""
-    // , "\n(exec PC0 (, (? $channel $payload $body) (! $channel $payload) (exec PC0 $p $t)) (, (body $body) (exec PC0_ $p $t)))"
-    , "\n(exec PC0 (, (? $channel $payload $body) (! $channel $payload) (exec PC0 $p $t)) (, ))"
-
-    // , "\n(exec PC1 (, (| $lprocess $rprocess) (exec PC1 $p $t)) (, $lprocess $rprocess (exec PC1 $p $t)))"
-    , "\n(? (add $ret) ((S $x) $y) (? (add $z) ($x $y) (! $ret (S $z)) ) )"
-    , "\n(? (add $ret) (Z $y) (! $ret $y))"
-    , "\n(! (add result) ((S Z) (S Z)))"
-    );
-
-    s.add_sexpr(SPACE_EXPRS.as_bytes(), expr!(s, "$"), expr!(s, "_1")).unwrap();
-
-    s.metta_calculus(100);
-
-    let mut v = vec![];
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"), &mut v).unwrap();
-
-    println!("\nRESULTS\n");
-    let res = String::from_utf8(v).unwrap();
-
-    assert_eq!(res.lines().count(), 3);
-    core::assert_eq!(
-        res,
-        "(! (add result) ((S Z) (S Z)))\n\
-         (? (add $) (Z $) (! _1 _2))\n\
-         (? (add $) ((S $) $) (? (add $) (_2 _3) (! _1 (S _4))))\n"
-    );
-
-    println!("{}", res);
-}
-
-#[test]
-fn metta_calculus_swap_0() {
-    let mut s = Space::new();
-
-    const SPACE_EXPRS: &str =
-    concat!
-    ( ""
-    , "\n(val a b)"
-    , "\n(exec (swap_0 \"00\") (, (val $x $y)) (, (swaped-val (val $x $y) (val $y $x))) )" // swap vals
-    , "\n(exec (swap_0 \"01\") (, (val $x $y)) (, (pair $x $y)) )" // swap vals
-    );
-
-    s.add_sexpr(SPACE_EXPRS.as_bytes(), expr!(s, "$"), expr!(s, "_1")).unwrap();
-
-    s.metta_calculus(100);
-
-    let mut writer = Vec::new();
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"), &mut writer).unwrap();
-
-    let out = String::from(std::str::from_utf8(&writer).unwrap());
-
-    // println!("\n{out:?}");
-    // println!("\n{:?}", s.dump_raw_at_root());
-
-    // println!("RESULTS:\n{}", out);
-    assert_eq!(out.lines().count(), 3);
-    assert_eq!(out, "(val a b)\n(pair a b)\n(swaped-val (val a b) (val b a))\n");
-}
-
-#[test]
-fn metta_calculus_swap2() {
-
-    let mut s = Space::new();
-
-    const SPACE_EXPRS: &str =
-    concat!
-    ( ""
-    , "\n(val a b)"
-    , "\n(val c d)"
-    , "\n(val e f)"
-    , "\n(val g h)"
-
-    , "\n(def (metta_thread_basic 2) (, (swapped $v $u))"
-    , "\n                            (, (val $v $u))"
-    , "\n)"
-
-    , "\n(exec (metta_thread_basic 1) (, (val $x $y) (def (metta_thread_basic 2) $p $t) )"
-    , "\n                         (,"
-    , "\n                            (swapped $y $x)"
-    , "\n                            (exec (metta_thread_basic 1) $p $t)"
-    , "\n                         )"
-    , "\n)"
-    );
-
-    s.add_sexpr(SPACE_EXPRS.as_bytes(), expr!(s, "$"), expr!(s, "_1")).unwrap();
-
-    s.metta_calculus(100);
-
-    let mut writer = Vec::new();
-    s.dump_sexpr(expr!(s, "$"), expr!(s, "_1"), &mut writer).unwrap();
-
-    let out = String::from(std::str::from_utf8(&writer).unwrap());
-
-    assert_eq!(out.lines().count(), 13);
-    assert_eq!(out,
-        "(val a b)\n\
-        (val b a)\n\
-        (val c d)\n\
-        (val d c)\n\
-        (val e f)\n\
-        (val f e)\n\
-        (val g h)\n\
-        (val h g)\n\
-        (swapped b a)\n\
-        (swapped d c)\n\
-        (swapped f e)\n\
-        (swapped h g)\n\
-        (def (metta_thread_basic 2) (, (swapped $ $)) (, (val _1 _2)))\n\
-        "
-    );
-
-    println!("RESULTS:\n{}", out);
-}
- */
-
-use std::ffi::OsStr;
-use std::ffi::OsString;
-use std::hash::{Hash, Hasher};
-use std::ops::Add;
-use base64::Engine;
-use serde::{Serialize, Deserialize};
-use clap::{Args, Parser as CLAParser, Subcommand, ValueEnum};
-use clap::builder::TypedValueParser;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 enum Format { MeTTa, JSON, CSV, UPaths, Paths, ACT }
@@ -2827,7 +2500,6 @@ fn main() {
     // mm1_forward();
     // mm2_bc();
     // sink_add_remove();
-    // sink_wasm_add();
     // bench_cm0(50);
     // return;
 
@@ -2838,8 +2510,8 @@ fn main() {
             #[cfg(debug_assertions)]
             println!("WARNING running in debug, if unintentional, build with --release");
             let mut selected: BTreeSet<&str> = only.split(",").collect();
-            if selected.remove("all") { selected.extend(&["counter_machine", "transitive", "clique", "finite_domain", "process_calculus", "exponential", "exponential_fringe", "odd_even_sort"]) }
             if selected.remove("default") { selected.extend(&["counter_machine", "transitive", "clique", "finite_domain", "process_calculus"]) }
+            if selected.remove("all") { selected.extend(&["counter_machine", "transitive", "clique", "finite_domain", "process_calculus", "exponential", "exponential_fringe", "odd_even_sort", "logic_query"]) }
             if selected.remove("sinks") { selected.extend(&["odd_even_sort"]) }
 
             for b in selected {
@@ -2853,6 +2525,7 @@ fn main() {
                     "exponential" => { exponential(32); }
                     "exponential_fringe" => { exponential_fringe(15); }
                     "odd_even_sort" => { bench_sink_odd_even_sort(2000); }
+                    "logic_query" => { bench_logic_query() }
                     s => { println!("bench not known: {s}") }
                 }
             }
@@ -2888,6 +2561,9 @@ fn main() {
             sink_count_literal();
             sink_count_constant();
             sink_count();
+
+            parse_csv();
+            parse_json();
         }
         Commands::Run { input_path, steps, instrumentation, output_path } => {
             #[cfg(debug_assertions)]
